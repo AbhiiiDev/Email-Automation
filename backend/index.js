22fc7ade4768a8e82e1dd0acebb82b2391d74299
ABHISHEK VERMA <104015898+AbhiiiDev@users.noreply.github.com>
2024-02-13 01:14:51 +0530

    final-attempt

--- backend/index.js
+++ backend/index.js
@@ -4,13 +4,9 @@ const cors=require('cors');
 
 const app=express();
 
-const corsOptions = {
-    origin: "https://email-automation-b1qu.vercel.app",
-    credentials: true, //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-  };
 
-app.use(cors(corsOptions));
+
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
